05f9910b6864c3eed77f3e1883d13a6fe7162b08
askatskov <160420092+askatskov@users.noreply.github.com>
2025-06-15 21:33:32 +0300

    Add project files.

---
+++ FLW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FLW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
